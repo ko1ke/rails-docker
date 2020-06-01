55d14660a6623d80aa597fa0b870e9a4b353dcf0
keke <dondohare55@gmail.com>
2020-06-01 21:06:37 +0900

    add cleaner

---
+++ docker/sh/exec,.sh
@@ -0,0 +1,4 @@
+docker-compose build
+docker-compose run app rails new . --force --no-deps --database=mysql --skip-test --webpacker
+echo "gem 'unicorn'" >> .Gemfile
+docker-compose run app bundle install -j4
