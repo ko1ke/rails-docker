368538d0cbe79ea18be1d2f96ddae6e93ece456b
keke <ryehkk@yahoo.co.jp>
2020-06-04 21:59:47 +0900

    add

--- docker/sh/exec.sh
+++ docker/sh/exec.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 docker-compose build
 docker-compose run app rails new . --force --no-deps --database=mysql --skip-test --webpacker
-echo "gem 'unicorn'" >> .Gemfile
+echo "gem 'unicorn'" >> Gemfile
 docker-compose run app bundle install -j4
+
+cp ./docker/rails/unicorn.rb ./config/
