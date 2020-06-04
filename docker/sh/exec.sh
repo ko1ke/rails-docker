9c1dbcdb27c34b45d0a1214731801f5f2fd55076
keke <ryehkk@yahoo.co.jp>
2020-06-04 22:13:57 +0900

    add

--- docker/sh/exec.sh
+++ docker/sh/exec.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 docker-compose build
-docker-compose run app rails new . --force --no-deps --database=mysql --skip-test --webpacker
+# docker-compose run app rails new . --force --no-deps --database=mysql --skip-test --webpacker
+docker-compose run app rails new . --force --no-deps --database=postgresql --skip-test --webpacker
 echo "gem 'unicorn'" >> Gemfile
 docker-compose run app bundle install -j4
 
